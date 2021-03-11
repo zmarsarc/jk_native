45c35e00d0e571d04787e8f0eaeb186267ef1eda
zengyu <zeng.yu.1993@gmail.com>
2021-03-11 16:57:04 +0800

    [ADD] 添加可停靠工具栏

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import sys
 from PySide6.QtWidgets import *
+from PySide6.QtCore import *
 from models.database import Database
 from models.jk_model import JKModel
 from controllers.jk_edit_controller import JKEditController
@@ -15,6 +16,7 @@ class DressShop(QMainWindow):
         self._create_menubar()
         self._create_statusbar()
         self._create_central_widget()
+        self._create_dock_tool_bar()
 
         self.setMinimumSize(800, 600)
 
@@ -35,6 +37,19 @@ class DressShop(QMainWindow):
         view.setItemDelegate(JKEditController())
         view.setModel(self._model)
         self.setCentralWidget(view)
+
+    def _create_dock_tool_bar(self):
+        toolbar = QDockWidget(self)
+        
+        button_container = QWidget(toolbar)
+        root = QHBoxLayout(button_container)
+        button_container.setLayout(root)
+        root.addWidget(QPushButton('new'))
+        root.addSpacerItem(QSpacerItem(20, 40, QSizePolicy.Expanding, QSizePolicy.Minimum))
+
+        toolbar.setWidget(button_container)
+        
+        self.addDockWidget(Qt.TopDockWidgetArea, toolbar)
 
 
 if __name__ == '__main__':
