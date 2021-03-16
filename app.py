81a5e3f467f4aeada3a10875d421c9bdb3b94f13
zengyu <zeng.yu.1993@gmail.com>
2021-03-16 14:31:45 +0800

    [FIX] 主窗口不显示的问题

--- app.py
+++ app.py
@@ -1,20 +1,12 @@
 import sys
-from PySide6.QtWidgets import *
-from PySide6.QtCore import *
+from PySide6.QtWidgets import QApplication
 from models.database import Database
-from gui.mainwindow import Ui_DressShopWindow
-
-class DressShop(QMainWindow):
-
-    def __init__(self, data: Database, parent=None):
-        super(DressShop, self).__init__(parent)
-        self.ui = Ui_DressShopWindow()
-        self.ui.setupUi(self)
+from views import DressShopMainWindow
 
 if __name__ == '__main__':
     data = Database('storage.db')
 
     app = QApplication(sys.argv)
-    shop = DressShop(data)
+    shop = DressShopMainWindow()
     shop.show()
     sys.exit(app.exec_())
