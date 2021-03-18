a1c86cb51c7cf1e817c76c6a2ebb891e6cd77eb8
zengyu <zeng.yu.1993@gmail.com>
2021-03-18 17:51:01 +0800

    [UPDATE] 使用新model

--- app.py
+++ app.py
@@ -2,13 +2,11 @@ import sys
 from PySide6.QtWidgets import QApplication
 from views import DressShopMainWindow
 from data.sqlite import SQLiteDriver
-import data
 
 if __name__ == '__main__':
     data_driver = SQLiteDriver('storage.db')
-    goods_data = data.Goods(data_driver)
 
     app = QApplication(sys.argv)
-    shop = DressShopMainWindow(goods_data)
+    shop = DressShopMainWindow(data_driver)
     shop.show()
     sys.exit(app.exec_())
--- models/goods.py
+++ models/goods.py
@@ -1,15 +1,16 @@
-from typing import Dict
+from typing import Dict, Optional, List
+from abc import abstractmethod
 from PySide6.QtCore import QAbstractTableModel, QModelIndex, Qt
+from datetime import datetime
 import data
 
-
-class GoodsType(object):
+class GoodsType:
 
     """
     商品类型类
 
     两个字段，是商品类型的两种表示。
-    
+
     code是数据表示，每种商品类型都有不重复的类型码，name是人类可读的表示，例如 JK Lo 小物等等。
     """
 
@@ -41,18 +42,99 @@ CodeToGoodsType: Dict[int, GoodsType] = {
 }
 
 
+class DataDriver(data.GoodsDataDriver, data.JKInventoryDataDriver):
+    pass
+
+class GoodsItem:
+    
+    def __init__(self, model: data.GoodsModel = None):
+        if model is not None:
+            self._data = model
+        else:
+            self._data = data.GoodsModel()
+        
+    @property
+    def id(self) -> int:
+        return self._data.id
+    
+    @property
+    def name(self) -> str:
+        return self._data.name
+    
+    @name.setter
+    def name(self, n: str):
+        self._data.name = n
+
+    @property
+    def type(self) -> GoodsType:
+        return CodeToGoodsType[self._data.type]
+    
+    @type.setter
+    def type(self, t: GoodsType):
+        #  todo: 如果商品已经创建了，就不能再修改其类型了
+        pass
+
+    @property
+    def create_time(self) -> datetime:
+        return self._data.create_time
+
+    @property
+    def comment(self) -> Optional[str]:
+        return self._data.comment
+
+    @abstractmethod
+    def save(self):
+        pass
+
+    @abstractmethod
+    def remove(self):
+        pass
+
+
+class JKItem(GoodsItem):
+
+    def __init__(self, driver: data.JKInventoryDataDriver, model: data.GoodsModel=None):
+        super(JKItem, self).__init__(model)
+        self._driver = driver
+
+    def save(self):
+        pass
+
+    def remove(self):
+        pass
+
+
+class AccessoiresItem(GoodsItem):
+
+    def __init__(self, model: data.GoodsModel=None):
+        super(AccessoiresItem, self).__init__(model)
+
+    def save(self):
+        pass
+
+    def remove(self):
+        pass
+
+
 class Goods(QAbstractTableModel):
 
     _header_data = ('名称', '类型', '创建时间', '备注')
 
-    def __init__(self, goodsdata: data.Goods, parent=None):
+    def __init__(self, driver: DataDriver, parent=None):
         super(Goods, self).__init__(parent)
-        self._goods_data = goodsdata
-        self._goods = None
+        self._data = driver
+        self._goods = self._load()
+
+    def _load(self) -> List[GoodsItem]:
+        goods: List[GoodsItem] = []
+        for g in self._data.all_goods():
+            if g.type == GoodsTypeJK.code:
+                goods.append(JKItem(self._data, g))
+            if g.type == GoodsTypeAccessoires.code:
+                goods.append(AccessoiresItem(g))
+        return goods
         
     def rowCount(self, parent=QModelIndex()):
-        if self._goods is None:
-            self._goods = self._goods_data.all()
         return len(self._goods)
 
     def columnCount(self, parent=QModelIndex()):
@@ -85,15 +167,12 @@ class Goods(QAbstractTableModel):
 
     def add_new_goods(self, gd):
         self.beginInsertRows(QModelIndex(), self.rowCount()+1, self.rowCount()+1)
-        g = self._goods_data.new()
+        g = self._data.new()
         g.name = gd['name']
         g.goods_type = gd['goods_type']
         g.comment = gd['comment']
         g.save()
 
-        self._goods = self._goods_data.all()
+        self._goods = self._data.all()
 
         self.endInsertRows()
-    
-    def types(self):
-        return data.Goods.TYPES
